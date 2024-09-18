1143a6cc4c9babb3517f66c319108dd7b543dee5
Aoxcis <gregoirepaul@laposte.net>
2024-09-18 18:01:46 +0200

    commencement prixTuring nbGagnant fonctionnel

--- TP-03-PrixTuring/prixTuring.c
+++ TP-03-PrixTuring/prixTuring.c
@@ -20,12 +20,56 @@
 // MAIN
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+struct gagnants {
+		int annee;
+		char nom[40];
+		char description[200];
+	};
+
+int numberOfWinners(FILE *fichier){
+	int counter = 0;
+	char *buffer[1024];
+	while(fgets(buffer, sizeof(buffer), fichier) != NULL){
+		counter++;
+	}
+	return counter;
+}
+
+char* readLine(FILE *fichier){
+	char *buffer[100];
+	fgets(buffer, sizeof(buffer), fichier);
+	return buffer;
+}
+
+char* readPartLine(FILE *fichier){
+	char *line = readLine(fichier);
+	char *buffer[50];
+	int current = 0;
+	while(fgetc(line) != ';' || fgetc(line) != EOF){
+		buffer[current] = fgetc(line);
+	}
+}	
+
+
 int main(int argc, char** argv)
 {
 	char filename[] = "turingWinners.csv";
 	char outputFilename[] = "out.csv";
 
-    // TODO
+	FILE *fichier = fopen(filename, "r");
+	if (fichier == NULL) {
+		fprintf(stderr, "Erreur: %s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+	else{
+		printf("Fichier ouvert avec succes\n");
+	}
+	int nb;
+	
+	nb = numberOfWinners(fichier);
 
+	printf("Number of winners : %d\n", nb);
+	fclose(fichier);
 	return EXIT_SUCCESS;
 }
+
