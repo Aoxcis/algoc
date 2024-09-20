f81a5bfb89059e86f6f40a60519770996d1bb240
Aoxcis <gregoirepaul@laposte.net>
2024-09-20 10:03:31 +0200

    number of winnners et readLine fonctionnent

--- TP-03-PrixTuring/prixTuring.c
+++ TP-03-PrixTuring/prixTuring.c
@@ -20,15 +20,16 @@
 // MAIN
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-struct gagnants {
+typedef struct {
 		int annee;
-		char nom[40];
-		char description[200];
-	};
+		char *nom;
+		char *description;
+	} Gagnant;
 
 int numberOfWinners(FILE *fichier){
 	int counter = 0;
-	char *buffer[1024];
+	char buffer[2000];
+	
 	while(fgets(buffer, sizeof(buffer), fichier) != NULL){
 		counter++;
 	}
@@ -36,19 +37,46 @@ int numberOfWinners(FILE *fichier){
 }
 
 char* readLine(FILE *fichier){
-	char *buffer[100];
+	rewind(fichier);
+	char buffer[2000];
 	fgets(buffer, sizeof(buffer), fichier);
 	return buffer;
 }
 
 char* readPartLine(FILE *fichier){
 	char *line = readLine(fichier);
-	char *buffer[50];
+	printf("ligne entiere : %s\n", line);
+	int buffer1[50];
+	char buffer2[50];
+	char buffer3[50];
 	int current = 0;
-	while(fgetc(line) != ';' || fgetc(line) != EOF){
-		buffer[current] = fgetc(line);
+	int partie = 0;
+	for(int i = 0; fgetc(fichier) == '\0'; i++){
+		if(fgetc(fichier) == ';' || fgetc(fichier) == '\0'){
+			current = 0;
+			partie++;
+		}
+		else {
+			if(partie == 0 && current < 49) {
+				buffer1[current] = fgetc(fichier);
+				buffer1[current + 1] = '\0';
+			}
+			if(partie == 1 && current < 49) {
+				buffer2[current] = fgetc(fichier);
+				buffer2[current + 1] = '\0';
+			}
+			if(partie == 2 && current < 49) {
+				buffer3[current] = fgetc(fichier);
+				buffer3[current + 1] = '\0';
+			}
+			current++;
+		}
+		if(partie >2){
+			break;
+		} 
 	}
-}	
+	return buffer1;
+}
 
 
 int main(int argc, char** argv)
@@ -65,10 +93,13 @@ int main(int argc, char** argv)
 		printf("Fichier ouvert avec succes\n");
 	}
 	int nb;
-	
 	nb = numberOfWinners(fichier);
-
 	printf("Number of winners : %d\n", nb);
+
+	char *prem;
+	prem = readLine(fichier);
+	printf("ligne entiere : %s", prem);
+	prem = NULL;
 	fclose(fichier);
 	return EXIT_SUCCESS;
 }
